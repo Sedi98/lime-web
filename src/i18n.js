b00c12a6d1d25343ccaaf93ba695c0bb89dc3317
Sedi98 <sedi9816@gmail.com>
2023-11-07 14:54:03 +0400

    saved last language at start

--- src/i18n.js
+++ src/i18n.js
@@ -16,7 +16,7 @@ i18n
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
     
-    lng: "az", // if you're using a language detector, do not define the lng option
+   
     fallbackLng: "az",
 
     interpolation: {
