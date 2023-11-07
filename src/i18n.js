65a6c17f8d7eef8eca3a5faca0a27e4884941e91
Sedi98 <sedi9816@gmail.com>
2023-11-07 14:56:46 +0400

    fix

--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,14 @@
 
 import i18n from "i18next";
 import {  initReactI18next } from "react-i18next";
+
+import detector from "i18next-browser-languagedetector";
 import Backend from 'i18next-http-backend';
 
 
 
 i18n
+.use(detector)
   .use(Backend) 
 
   .use(initReactI18next) // passes i18n down to react-i18next
